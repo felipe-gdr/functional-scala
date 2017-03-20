6e2b0de3f57cc2fdfbdfae3a88eb267a5dd76fb6
Felipe Reis <felipe@glr.com.br>
2017-03-19 21:08:18 -0300

    feat: Solve balance parenthesis problem

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,17 +13,33 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = 
-      if (c==0 || c == r) 1
-      else pascal(c-1,r-1) + pascal(c,r-1)
-  
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+  def balance(chars: List[Char]): Boolean = {
+    def move(opened: Int, remain: List[Char]): Boolean = {
+      if (opened < 0) false
+      else if (remain.isEmpty) opened == 0
+      
+      else {
+        def head = remain.head
+        def tail = remain.tail
+
+        if (head == '(') move(opened + 1, tail)
+        else if (head == ')') move(opened - 1, tail)
+        else move(opened, tail)
+      }
+    }
+
+    move(0, chars)
+  }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
